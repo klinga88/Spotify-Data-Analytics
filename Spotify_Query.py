ea5ae861e632022bb28fd2aba14f579510c8225e
klinga <klinga88@gmail.com>
2018-08-22 19:57:24 -0400

    first push

---
+++ Spotify_Query.py
@@ -0,0 +1,15 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_credentials_manager = SpotifyClientCredentials(client_id='**YOURCLIENTID**', client_secret='**YOURCLIENTSECRET')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlists = sp.user_playlists('**USER**')
+
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
